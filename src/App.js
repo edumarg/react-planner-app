6bce3cf2cbef38d1876ced541c05e6ec3683846c
edumarg <edumarg@gmx.com>
2020-07-22 17:01:21 +0300

    Finished building done list framework

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import AddBar from "./components/addBar";
 import ToDoList from "./components/toDoList";
+import DoneList from "./components/DoneList";
 
 function App() {
   return (
     <React.Fragment>
       <AddBar> </AddBar>
       <ToDoList></ToDoList>
+      <DoneList></DoneList>
     </React.Fragment>
   );
 }
